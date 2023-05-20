8d4fb53e86cd3f39eff94781de56716cf66321d4
Kateryna <kateryna.yakubych@gmail.com>
2023-05-20 12:25:20 +0300

    add something in 3 task

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,3 +1,25 @@
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+
+const form = document.querySelector(".form");
+const refs = {
+  inputFirstDelay: document.querySelector("[name='delay']"),
+  inputDelayStep: document.querySelector("[name='step']"),
+  inputAmount: document.querySelector("[name='amount']"),
+}
+
+form.addEventListener("submit", onFormSubmit);
+
+
+function onFormSubmit(event) {
+  console.log("escfneksnfclen");
+  event.preventDefault();
+
+
+  let firstDelay = refs.inputFirstDelay.value;
+  
+  console.log(firstDelay);
+}
+ 
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
   if (shouldResolve) {
